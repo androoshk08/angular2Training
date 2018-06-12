059dc5251f7a5b6a8a6db28cdb96c6a0b1284e17
androoshk08 <androoshk08@gmail.com>
2018-06-12 09:25:26 +0300

    first commit

--- myTutorial/src/app/voter-child/voter-child.component.ts
+++ myTutorial/src/app/voter-child/voter-child.component.ts
@@ -1,19 +1,35 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-voter-child',
   templateUrl: './voter-child.component.html',
   styleUrls: ['./voter-child.component.css']
 })
-export class VoterChildComponent implements OnInit {
+export class VoterChildComponent implements OnInit, OnDestroy {
+  intervalId= 0;
+  message = '';
+  seconds = 11;
 
   @Input() name: string;
   @Output() onVoted = new EventEmitter<boolean>();
   voted = false;
 
+  testValue = '';
+  @Input()
+  get test() {
+    return this.testValue;
+  }
+  set test(val) {
+    this.testValue = val;
+    this.testChange.emit(this.testValue);
+  }
+
+  @Output() testChange = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+    this.start();
   }
 
   vote(agreed: boolean) {
@@ -21,4 +37,36 @@ export class VoterChildComponent implements OnInit {
     this.voted =true;
   }
 
+  clearTimer() {
+    clearInterval(this.intervalId);
+  }
+
+  start() {
+    this.countDown();
+  }
+
+  stop() {
+    this.clearTimer();
+    this.message = `Holding at T-${this.seconds} seconds`;
+  }
+
+  private countDown() {
+    this.clearTimer();
+    this.intervalId = window.setInterval(() =>{
+      this.seconds -=1;
+      if (this.seconds === 0) {
+        this.message = 'Blast off!'
+      }
+      else {
+        if (this.seconds < 0) {
+          this.seconds = 10; //reset
+        }
+        this.message = `T-${this.seconds} seconds and counting`;
+      }
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    this.clearTimer();
+  }
 }
--- myTutorial/src/app/voter-parent/voter-parent.component.ts
+++ myTutorial/src/app/voter-parent/voter-parent.component.ts
@@ -1,11 +1,15 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {VoterChildComponent} from "../voter-child/voter-child.component";
 
 @Component({
   selector: 'app-voter-parent',
   templateUrl: './voter-parent.component.html',
   styleUrls: ['./voter-parent.component.css']
 })
-export class VoterParentComponent implements OnInit {
+export class VoterParentComponent implements OnInit, AfterViewInit {
+
+  @ViewChild (VoterChildComponent)
+  private timerComponent:  VoterChildComponent;
   agreed = 0;
   disagreed = 0;
   voters = ['Mr IQ', 'Ms Universe', 'Bombasto'];
@@ -13,6 +17,13 @@ export class VoterParentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  seconds() {
+    return 0;
+  }
+  ngAfterViewInit() {
+    setTimeout(() => this.seconds = () => this.timerComponent.seconds, 0);
   }
 
   onVoted(agreed: boolean) {
