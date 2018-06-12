dcfa880356053549b618070011acf87bfcccd5fd
androoshk08 <androoshk08@gmail.com>
2018-06-12 09:56:54 +0300

    first commit

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,14 @@
 /**
  * Created by training on 6/12/2018.
  */
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(_dirname)); //setare pentru cale relativa
+
+app.listen(3001, function() {
+    console.log('Hey!');
+});
